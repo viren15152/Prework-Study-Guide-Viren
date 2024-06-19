f4d41afea4be3f7ce0c972d5f7d5c9e94158b319
Viren <144032304+viren15152@users.noreply.github.com>
2024-06-19 16:41:14 +0100

    Added JavaScript functions and HTML

--- assets/script.js
+++ assets/script.js
@@ -24,6 +24,6 @@ function selectTopic() {
 console.log('Here are the topics we learned through Prework:');
 listTopics();
 console.log('Which topic should we study first?');
-selectTopic();
+selectTopic();s
 
 
